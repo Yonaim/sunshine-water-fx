d026a638e9f5aa72a02baaeccced6a373e32e2cc
yonaMac <yonaimyo@gmail.com>
2025-07-10 15:56:17 +0900

    Feat: skeleton of main

--- main.js
+++ main.js
@@ -1,100 +1,99 @@
 import { OrbitCamera } from './camera.js';
-
-async function fetchText(url) {
-  return await (await fetch(url)).text();
-}
-
-function createShader(gl, type, src) {
-  const shader = gl.createShader(type);
-  gl.shaderSource(shader, src);
-  gl.compileShader(shader);
-  if (!gl.getShaderParameter(shader, gl.COMPILE_STATUS)) throw gl.getShaderInfoLog(shader);
-  return shader;
-}
-function createProgram(gl, vsrc, fsrc) {
-  const vs = createShader(gl, gl.VERTEX_SHADER, vsrc);
-  const fs = createShader(gl, gl.FRAGMENT_SHADER, fsrc);
-  const prog = gl.createProgram();
-  gl.attachShader(prog, vs); gl.attachShader(prog, fs);
-  gl.linkProgram(prog);
-  if (!gl.getProgramParameter(prog, gl.LINK_STATUS)) throw gl.getProgramInfoLog(prog);
-  return prog;
-}
+import { createManualMipmapTexture } from './mipmap.js';
+import { mat4 } from "https://cdn.jsdelivr.net/npm/gl-matrix@3.4.3/+esm";
+import { Skybox, skyboxFS, skyboxVS } from './skybox.js';
+import { createProgram } from './utils.js';
+import { loadCubemap } from './cubemap.js';
+import { planeFS, planeVS, WaterPlane } from './WaterPlane.js';
 
 const canvas = document.getElementById('webgl');
 const gl = canvas.getContext('webgl2');
 if (!gl) throw 'WebGL2 not supported';
 
-const [planeVS, planeFS] = await Promise.all([
-  fetchText('shaders/waterEffect.vert'),
-  fetchText('shaders/waterEffect.frag'),
-]);
-
+// ======================== Objects ========================
+const faceInfos = [
+    { target: gl.TEXTURE_CUBE_MAP_POSITIVE_X, url: 'skybox/px.bmp' },
+    { target: gl.TEXTURE_CUBE_MAP_NEGATIVE_X, url: 'skybox/nx.bmp' },
+    { target: gl.TEXTURE_CUBE_MAP_POSITIVE_Y, url: 'skybox/py.bmp' },
+    { target: gl.TEXTURE_CUBE_MAP_NEGATIVE_Y, url: 'skybox/ny.bmp' },
+    { target: gl.TEXTURE_CUBE_MAP_POSITIVE_Z, url: 'skybox/pz.bmp' },
+    { target: gl.TEXTURE_CUBE_MAP_NEGATIVE_Z, url: 'skybox/nz.bmp' },
+  ];
+const cubemap = await loadCubemap(gl, faceInfos);
 const camera = new OrbitCamera();
-
+const skyboxProg = createProgram(gl, skyboxVS, skyboxFS);
 const planeProg = createProgram(gl, planeVS, planeFS);
-const planeBuf = gl.createBuffer();
-gl.bindBuffer(gl.ARRAY_BUFFER, planeBuf);
-gl.bufferData(gl.ARRAY_BUFFER, new Float32Array([
-  -1,-1,  1,-1,  -1,1,
-   1,-1,  1, 1,  -1,1
-]), gl.STATIC_DRAW);
+const skybox = new Skybox(gl, skyboxProg, cubemap);
+const planeScale = 1.0;
+const waterPlane = new WaterPlane(gl, planeProg, planeScale);
 
-function createTexture(gl, url, unit) {
-  const tex = gl.createTexture();
-  gl.activeTexture(gl.TEXTURE0 + unit);
-  gl.bindTexture(gl.TEXTURE_2D, tex);
-  gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGB, 1, 1, 0, gl.RGB, gl.UNSIGNED_BYTE, new Uint8Array([255,255,255]));
-  const image = new Image();
-  image.onload = () => {
-    gl.activeTexture(gl.TEXTURE0 + unit);
-    gl.bindTexture(gl.TEXTURE_2D, tex);
-    gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGB, gl.RGB, gl.UNSIGNED_BYTE, image);
-    gl.generateMipmap(gl.TEXTURE_2D);
-  };
-  image.src = url;
-  return tex;
-}
-createTexture(gl, 'mipmap/L1.png', 0);
-
-function perspective(out, fovy, aspect, near, far) {
-  const f = 1.0 / Math.tan(fovy/2), nf = 1/(near-far);
-  out[0]=f/aspect; out[1]=0; out[2]=0; out[3]=0;
-  out[4]=0; out[5]=f; out[6]=0; out[7]=0;
-  out[8]=0; out[9]=0; out[10]=(far+near)*nf; out[11]=-1;
-  out[12]=0; out[13]=0; out[14]=2*far*near*nf; out[15]=0;
-  return out;
-}
-
+// ======================== Rendering ========================
 const baseColor = [0.10, 0.65, 1.0];
-
-function render(time) {
-    const loc = gl.getUniformLocation(planeProg, 'baseColor');
- gl.uniform3fv(loc, baseColor);
-  gl.viewport(0,0,gl.canvas.width,gl.canvas.height);
-  gl.clearColor(baseColor[0], baseColor[1], baseColor[2], 1.0);  // 캔버스 파란색으로
-  gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+const mipmapFiles = [
+    'mipmap/L0.png', // level 0 (original)
+    'mipmap/L1.png', // level 0 (1/2)
+    'mipmap/L2.png', // level 1 (1/4)
+    'mipmap/L3.png', // level 2 (1/8)
+    'mipmap/L4.png' // level 3 (1/16)
+];
+let waveTex = null;
+createManualMipmapTexture(gl, mipmapFiles, 0).then(tex => {
+    gl.bindTexture(gl.TEXTURE_2D, tex);
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR_MIPMAP_LINEAR);
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.LINEAR);
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_BASE_LEVEL, 0);
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAX_LEVEL, mipmapFiles.length - 1);
+    waveTex = tex;
+    requestAnimationFrame(render);
+  });
   
 
-  // ----- Plane (water) -----
-  gl.useProgram(planeProg);
-  gl.bindBuffer(gl.ARRAY_BUFFER, planeBuf);
-  const planePosLoc = gl.getAttribLocation(planeProg,'a_position');
-  gl.enableVertexAttribArray(planePosLoc);
-  gl.vertexAttribPointer(planePosLoc,2,gl.FLOAT,false,0,0);
+function render(time) {
+    gl.viewport(0,0,gl.canvas.width,gl.canvas.height);
+    if (!waveTex) return;
+    gl.clearColor(baseColor[0], baseColor[1], baseColor[2], 1.0);
+    gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+  
+    // ---------- Camera ----------
+    const fovy = Math.PI / 4;
+    const aspect = canvas.width / canvas.height;
+    const near = 0.1;
+    const far = 100.0;
+    
+    // -------- Matrices ----------
+    const model = mat4.create();
+    const view = camera.getViewMatrix();
+    const proj = mat4.create();
+    const viewNoTrans = camera.getSkyboxViewMatrix(); // for skybox
+    viewNoTrans[12] = 0; viewNoTrans[13] = 0; viewNoTrans[14] = 0;
+    mat4.perspective(proj, fovy, aspect, near, far);
+    const mvp = mat4.create();
+    mat4.multiply(mvp, proj, view);
+  
+    // ----- Skybox -----
+    // 1. FBO 준비
+    const fbo = gl.createFramebuffer();
+    const fboTex = gl.createTexture();
+    gl.bindTexture(gl.TEXTURE_2D, fboTex);
+    gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, canvas.width, canvas.height, 0, gl.RGBA, gl.UNSIGNED_BYTE, null);
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR);
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.LINEAR);
+    gl.bindFramebuffer(gl.FRAMEBUFFER, fbo);
+    gl.framebufferTexture2D(gl.FRAMEBUFFER, gl.COLOR_ATTACHMENT0, gl.TEXTURE_2D, fboTex, 0);
 
-  const view = camera.getViewMatrix();
-  const proj = perspective(new Float32Array(16), Math.PI/3, gl.canvas.width/gl.canvas.height, 0.1, 100.0);
+    // 2. skybox 렌더링 (FBO, Screen 각각)
+    skybox.draw(viewNoTrans, proj, fbo, canvas.width, canvas.height);
+    skybox.draw(viewNoTrans, proj);
 
-  gl.uniformMatrix4fv(gl.getUniformLocation(planeProg,'u_view'),false,view);
-  gl.uniformMatrix4fv(gl.getUniformLocation(planeProg,'u_proj'),false,proj);
-  gl.uniform1f(gl.getUniformLocation(planeProg,'u_time'), time*0.001);
-  gl.uniform1f(gl.getUniformLocation(planeProg,'u_zoom'),1.0);
-
-  gl.uniform1i(gl.getUniformLocation(planeProg,'waveTex'), 0);
-
-  gl.drawArrays(gl.TRIANGLES,0,6);
-
-  requestAnimationFrame(render);
+    // ----- waterPlane -----
+    waterPlane.draw(
+        waveTex,
+        time * 0.003,
+        mvp,
+        2.0,
+        fboTex,        // skybox FBO texture
+        [canvas.width, canvas.height]
+    );
+    
+    requestAnimationFrame(render);
 }
-requestAnimationFrame(render);
