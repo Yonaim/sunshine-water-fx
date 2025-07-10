aaec72af9e673636f3a64948bed4657caabe31bb
yonaMac <yonaimyo@gmail.com>
2025-07-10 15:55:41 +0900

    Feat: WaterPlane

---
+++ WaterPlane.js
@@ -0,0 +1,54 @@
+import { fetchText } from "./utils.js";
+
+export const [planeVS, planeFS] = await Promise.all([
+  fetchText('shaders/waterEffect.vert'),
+  fetchText('shaders/waterEffect.frag'),
+]);
+
+export class WaterPlane {
+    constructor(gl, program, scale=1.0) {
+        this.gl = gl;
+        this.program = program;
+        this.scale = scale;
+        this.verts = [
+            -scale, -1, -scale,
+             scale, -1, -scale,
+            -scale, -1,  scale,
+            -scale, -1,  scale,
+             scale, -1, -scale,
+             scale, -1,  scale
+        ];
+
+        this.buf = gl.createBuffer();
+        gl.bindBuffer(gl.ARRAY_BUFFER, this.buf);
+        gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(this.verts), gl.STATIC_DRAW);
+    }
+    draw(waveTex, time, mvp, lodBias, screenColorTex, screenSize) {
+        const gl = this.gl;
+        gl.useProgram(this.program);
+    
+        gl.bindBuffer(gl.ARRAY_BUFFER, this.buf);
+        gl.enableVertexAttribArray(0);
+        gl.vertexAttribPointer(0, 3, gl.FLOAT, false, 0, 0);
+    
+        gl.activeTexture(gl.TEXTURE0);
+        gl.bindTexture(gl.TEXTURE_2D, waveTex);
+        gl.uniform1i(gl.getUniformLocation(this.program, 'waveTex'), 0);
+    
+        gl.activeTexture(gl.TEXTURE2);
+        gl.bindTexture(gl.TEXTURE_2D, screenColorTex);
+        gl.uniform1i(gl.getUniformLocation(this.program, 'screenColorTex'), 2);
+    
+        gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
+        gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
+    
+        gl.uniform1f(gl.getUniformLocation(this.program, 'u_scale'), this.scale);
+        gl.uniform1f(gl.getUniformLocation(this.program, 'u_time'), time);
+        gl.uniformMatrix4fv(gl.getUniformLocation(this.program, 'u_modelViewProjection'), false, mvp);
+        gl.uniform1f(gl.getUniformLocation(this.program, 'lodBias'), lodBias);
+        gl.uniform2f(gl.getUniformLocation(this.program, 'screenSize'), screenSize[0], screenSize[1]);
+    
+        gl.drawArrays(gl.TRIANGLES, 0, 6);
+    }
+    
+}
